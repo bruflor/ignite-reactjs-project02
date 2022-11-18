85b9ccf98c21d2adcd6b66746c1ff878804dd885
Bruna Flor <brn.flor@gmail.com>
2022-11-18 15:16:59 +0000

    global colors

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -12,7 +12,7 @@ interface ButtonContainerProps {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
 width: 100px;
 height: 40px;
-background-color: ${props => props.theme.primary};
+background-color: ${props => props.theme["green-500"]};
 color: ${props => props.theme.white}
 
 `
