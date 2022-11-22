2aa18896cf338e9c2300b550f59c27a7f99e5720
Bruna Flor <brn.flor@gmail.com>
2022-11-22 12:43:08 +0000

    5 in 5

--- src/pages/Home/components/NewCycleForm/index.tsx
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -32,7 +32,7 @@ export const NewCycleForm = () => {
         type="number"
         placeholder="00"
         step={5}
-        min={1}
+        min={5}
         max={60}
         disabled={!!activeCycle}
         {...register("minutesAmount", { valueAsNumber: true })}
